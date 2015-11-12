a6f2a4196370496534e95637e94eba687bd19372
jaxgeller <jacksongeller@gmail.com>
2015-11-11 23:12:05 -0500

    hide players that went undrafted

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -1,5 +1,8 @@
 import "./d3.js"
 
+run(2007);
+
+
 function setData(data) {
   console.log(data)
   document.querySelector('.meta-head-shot').src = data.img;
@@ -17,72 +20,85 @@ function setData(data) {
   document.getElementById('pos').textContent = data.pos;
 }
 
-
-const margin = {top: 0, right: 30, bottom: 0, left: 50};
-const width = 1060 - margin.left - margin.right;
-const height = 2000 - margin.top - margin.bottom;
-
-let svg = d3.select('#chart')
-  .append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-  .append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
-
-let sankey = d3.sankey()
-  .nodeWidth(0)
-  .nodePadding(0)
-  .size([width, height]);
-
-let path = sankey.link();
-
-d3.json("data/2007.json", function(energy) {
-  sankey
-      .nodes(energy.nodes)
-      .links(energy.links)
-      .layout(0);
-
-  var link = svg.append("g").selectAll(".link")
-      .data(energy.links)
-      .enter().append("path")
-        .attr("class", "link")
-        .attr("d", path)
-        .attr('stroke', function(d) {
-          if (d.picked > d.source.node-60)
-            return '#d0a180';
-          else
-            return '#06904f';
-        })
-        .style("stroke-width", function(d) {
-          if (d.picked < 0) return 0;
-          return 2.5;
-        })
-        .on('mouseover', function() {
-          setData(this.__data__)
-        })
-
-  var node = svg.append("g").selectAll(".node")
-      .data(energy.nodes)
-      .enter().append("g")
-        .attr("class", "node")
-        .attr("transform", function(d) {
-          var x = d.x;
-          var y = d.y;
-          if (x > 900)
-            x = d.x + margin.right - 10;
-          else
-            x = d.x - margin.left;
-          y = d.y + 6.5;
-          return "translate(" + x + "," + y + ")";
-        })
-
-  node.append("rect")
-      .attr("height", function(d) { return 1; })
-      .attr("width", function(d) {
-        if (d.x > 900)
-          return 10;
-        return 20;
-      })
-      .style("fill", function(d) { return '#cfd8dc' })
-      .style("stroke", function(d) { return '#cfd8dc' })
+[].slice.call(document.querySelectorAll('button')).forEach(button=> {
+  button.addEventListener('click', function() {
+    document.getElementById('chart').innerHTML = ''
+    run(this.textContent)
+  });
 });
+
+
+function run(year) {
+  const margin = {top: 0, right: 30, bottom: 0, left: 50};
+  const width = 1060 - margin.left - margin.right;
+  const height = 2000 - margin.top - margin.bottom;
+
+  let svg = d3.select('#chart')
+    .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+  let sankey = d3.sankey()
+    .nodeWidth(0)
+    .nodePadding(0)
+    .size([width, height]);
+
+  let path = sankey.link();
+
+
+  d3.json(`data/${year}.json`, function(energy) {
+    sankey
+        .nodes(energy.nodes)
+        .links(energy.links)
+        .layout(0);
+
+    var link = svg.append("g").selectAll(".link")
+        .data(energy.links)
+        .enter().append("path")
+          .attr("class", "link")
+          .attr("d", path)
+          .attr('stroke', function(d) {
+            if (d.picked > d.source.node-60)
+              return '#d0a180';
+            else
+              return '#06904f';
+          })
+          .style("stroke-width", function(d) {
+            if (d.picked < 0) return 0;
+            return 2.5;
+          })
+          .style('display', function(d) {
+            if (d.picked < 0) return 'none';
+          })
+          .on('mouseover', function() {
+            setData(this.__data__)
+          })
+
+    var node = svg.append("g").selectAll(".node")
+        .data(energy.nodes)
+        .enter().append("g")
+          .attr("class", "node")
+          .attr("transform", function(d) {
+            var x = d.x;
+            var y = d.y;
+            if (x > 900)
+              x = d.x + margin.right - 10;
+            else
+              x = d.x - margin.left;
+            y = d.y + 6.5;
+            return "translate(" + x + "," + y + ")";
+          })
+
+    node.append("rect")
+        .attr("height", function(d) { return 1; })
+        .attr("width", function(d) {
+          if (d.x > 900)
+            return 10;
+          return 20;
+        })
+        .style("fill", function(d) { return '#cfd8dc' })
+        .style("stroke", function(d) { return '#cfd8dc' })
+  });
+}
